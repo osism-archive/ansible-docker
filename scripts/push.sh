ec935d05685ede650571e78c7e2d37ca3b3b0280
Christian Berendt <berendt@betacloud-solutions.de>
2017-02-09 18:22:31 +0100

    Add Jenkinsfile

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,7 +4,7 @@ set -x
 # This file is subject to the terms and conditions defined in file 'LICENSE',
 # which is part of this repository.
 
-# Available environment variables:
+# Available environment variables
 #
 # NEXUS_ARTIFACT_REPOSITORY
 # NEXUS_ARTIFACT_NAME
@@ -12,8 +12,15 @@ set -x
 # NEXUS_USERNAME
 # NEXUS_PASSWORD
 # NEXUS_SERVER
+#
+# NEXUSFILE
 
-# Set default values:
+# Set default values
+
+NEXUSFILE=${NEXUSFILE:-nexus}
+if [[ -e $NEXUSFILE ]]; then
+    source $NEXUSFILE
+fi
 
 ARCHIVE=$NEXUS_ARTIFACT_NAME-$NEXUS_ARTIFACT_TAG.tar.gz
 
