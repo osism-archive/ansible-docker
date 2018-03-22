9fb56bd98264b323025ab3e5265ecf63fc18084f
Christian Berendt <berendt@betacloud-solutions.de>
2018-03-22 13:20:33 +0100

    molecule: add testinfra

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,10 @@
+def test_apt_preferences_docker_compose_file(host):
+    f = host.file("/etc/apt/preferences.d/docker-compose")
+    assert f.exists
+    assert f.is_file
+
+
+def test_apt_preferences_docker_file(host):
+    f = host.file("/etc/apt/preferences.d/docker")
+    assert f.exists
+    assert f.is_file
