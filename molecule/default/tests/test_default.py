d5f3671696e43f0bed1ed297f693cff45b2668d6
Jeremias Broedel <broedel@b1-systems.de>
2018-03-26 14:32:12 +0200

    testinfra: add tests for services/socket file

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -20,3 +20,18 @@ def test_limits_file(host):
     f = host.file("/etc/security/limits.d/docker.conf")
     assert f.exists
     assert f.is_file
+
+
+def test_docker_running_and_enabled(host):
+    f = host.service("docker")
+    assert f.is_running
+    assert f.is_enabled
+
+
+def test_docker_socket(host):
+    f = host.file("/var/run/docker.sock")
+    assert f.exists
+    assert f.is_socket
+
+    o = host.socket("unix:///var/run/docker.sock")
+    assert o.is_listening
