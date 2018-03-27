d9dc36aaedd11d8f1c4e0adb3c33b41036f9d06f
Jeremias Broedel <jeremias@gecko.suse>
2018-03-27 11:13:52 +0200

    added new test is storage mountpoint and owner available

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -37,7 +37,17 @@ def test_docker_socket(host):
     assert o.is_listening
 
 
-def test_docker_storage(host):
+def test_docker_storage_mountpoint_owner_mode(host):
+    f = host.file("/var/lib/docker")
+    assert f.exists
+    assert f.is_directory
+    assert not f.is_file
+    assert f.user == "root"
+    assert f.uid == 0
+    assert f.gid == 0
+    assert f.group == "root"
+    assert f.mode == 0o711
+
     f = host.mount_point('/var/lib/docker')
     assert f.exists
     assert isinstance(f.options, list)
@@ -46,6 +56,5 @@ def test_docker_storage(host):
 
     mountpoints = host.mount_point.get_mountpoints()
     assert mountpoints
-    assert all(isinstance(m, host.mount_point) for m in mountpoints)
-    assert len([m for m in mountpoints if m.path == "/var/lib/docker"]) == 1
-
+    assert all(isinstance(t, host.mount_point) for t in mountpoints)
+    assert len([t for t in mountpoints if t.path == "/var/lib/docker"]) == 1
