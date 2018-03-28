8d5cecc598f6b6157d9ea6cedd4fc23d63137aa9
Christian Berendt <berendt@betacloud-solutions.de>
2018-03-28 13:35:44 +0200

    testinfra: fix socket tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -22,19 +22,27 @@ def test_limits_file(host):
     assert f.is_file
 
 
-def test_docker_running_and_enabled(host):
+def test_docker_service(host):
     f = host.service("docker")
     assert f.is_running
     assert f.is_enabled
 
 
-def test_docker_socket(host):
+def test_docker_socket_service(host):
+    f = host.service("docker.socket")
+    assert f.is_running
+    assert f.is_enabled
+
+
+def test_docker_socket_unix(host):
     f = host.file("/var/run/docker.sock")
     assert f.exists
     assert f.is_socket
 
-    o = host.socket("unix:///var/run/docker.sock")
-    assert o.is_listening
+
+def test_docker_socket_tcp(host):
+    s = host.socket("tcp://127.0.0.1:2375")
+    assert s.is_listening
 
 
 def test_docker_user_group(host):
